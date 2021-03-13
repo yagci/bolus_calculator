5d1456d03acf569aa12fda2b4ae375ce6f13d489
yagci <yagci@users.noreply.github.com>
2021-03-13 15:51:56 +0100

    add github link, remove sources, fix typo in js

--- static/js/calculator.js
+++ static/js/calculator.js
@@ -1,9 +1,9 @@
 function calculate() {
-    let carbohyradtes = document.getElementById('ch-100').value;
+    let carbs = document.getElementById('ch-100').value;
     let portion = document.getElementById('portion-size').value;
 
-    let ke = (carbohyradtes / 100 * portion) / 10;
-    let be = (carbohyradtes / 100 * portion) / 12;
+    let ke = (carbs / 100 * portion) / 10;
+    let be = (carbs / 100 * portion) / 12;
 
     document.getElementById('ke').innerHTML = String(ke.round(1)) + '<span>CP</span>';
     document.getElementById('be').innerHTML = String(be.round(1)) + '<span>BE</span>';
